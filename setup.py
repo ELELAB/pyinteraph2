b0aa683928c7b351f18c4371afb5c272c911afe0
Matteo Tiberti <matteo.tiberti@gmail.com>
2020-08-20 09:52:21 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, Extension
-import numpy
 
-c_module = Extension('libinteract.innerloops', ['src/innerloops.c', 'src/clibinteract.c'])
+libinteract = Extension('libinteract.innerloops', ['libinteract/innerloops.pyx', 'libinteract/clibinteract.c'])
 
 setup(name = 'pyinteraph',
       url='http://linux.btbs.unimib.it/pyinteraph/',
@@ -11,7 +10,7 @@ setup(name = 'pyinteraph',
       author_email="matteo.tiberti@gmail.com",
       version = '1.1',
       description = 'Compute interatomic interactions in protein ensembles',
-      ext_modules = [c_module],
+      ext_modules = [libinteract],
       package_data = {'pyinteraph' : ['charged_groups.ini',  'hydrogen_bonds.ini', 'kbp_atomlist', 'charged_groups.ini', 'hydrogen_bonds.ini', 'ff.S050.bin64', 'ff_masses/*']},
       package_dir={'libinteract':'libinteract', 'pyinteraph':'pyinteraph'},
       packages=['libinteract', 'pyinteraph'],
@@ -20,5 +19,12 @@ setup(name = 'pyinteraph',
                          "graph_analysis = pyinteraph.graph_analysis:main",
                          "filter_graph = pyinteraph.filter_graph:main"] 
                    },
-      include_dirs=[numpy.get_include()]
+      install_requires=["biopython",
+                        "MDAnalysis==1.0.0",
+                        "numpy",
+                        "matplotlib",
+                        "networkx",
+                        "scipy",
+                        "pytest"],
+      setup_requires=["numpy"]
       )
