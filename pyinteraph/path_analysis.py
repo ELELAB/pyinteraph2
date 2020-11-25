78fd11026785942bd5718428fac0c240eca568ba
mahdi-robbani <mahdi.robbani@gmail.com>
2020-11-25 21:16:14 +0100

    Added source and target options

--- pyinteraph/path_analysis.py
+++ pyinteraph/path_analysis.py
@@ -17,41 +17,47 @@ def get_shortest_paths(graph, source, target, maxl, res_space):
     for node1, node2 in combinations:
         try:
             # Get a list of shortest paths and append to output
-            path = list(nx.algorithms.shortest_paths.generic.all_shortest_paths(graph, node1, node2))
+            path = list(nx.algorithms.shortest_paths.generic.all_shortest_paths(\
+                            G = graph, \
+                            source = node1, \
+                            target = node2))
             for p in path:
                 # Check that path is not longer than the maximum allowed length
                 if len(p) <= maxl:
                     paths.append(p)
         except nx.NetworkXNoPath:
+            pass
             # If no path is found log info
             log.info(f"No path found between {node1} and {node2}")
     return paths
 
 def sort_paths(graph, paths, sort_by):
+    """Takes in a list of paths and sorts them."""
+
     # Calculate length of path
     lengths = [len(p) for p in paths]
     # Calculate weights of path
-    weights = [[graph[path[i]][path[i+1]]["weight"] for i in range(len(path)-1)] for path in paths]
-    cum_weights = [np.sum(w) for w in weights]
+    weights = \
+    [[graph[p[i]][p[i+1]]["weight"] for i in range(len(p)-1)] for p in paths]
+    sum_weights = [np.sum(w) for w in weights]
     avg_weights = [np.mean(w) for w in weights]
     # Sort paths
-    paths = zip(paths, lengths, avg_weights, cum_weights)
+    paths = zip(paths, lengths, sum_weights, avg_weights)
     if sort_by == "length":
         key = lambda x: x[1]
         reverse = False
-    elif sort_by == "average_weight":
+    elif sort_by == "cumulative_weight":
         key = lambda x: x[2]
         reverse = True
-    elif sort_by == "cumulative_weight":
+    elif sort_by == "average_weight":
         key = lambda x: x[3]
         reverse = True
     sorted_paths = sorted(paths, key = key, reverse = reverse)
     return sorted_paths
 
 
-
-        
 def main():
+    ######################### ARGUMENT PARSER #########################
     description = "Find shortest path"
     parser = argparse.ArgumentParser(description= description)
 
@@ -90,14 +96,38 @@ def main():
                         help = \
                             s_helpstr.format(", ".join(s_choices), s_default))
 
-    # add target option
+    a_helpstr = "Source residue for paths calculation (see option -p)"
+    parser.add_argument("-a", "--source",
+                        dest = "source",
+                        default = None,
+                        type = int,
+                        help = a_helpstr)
+
+    b_helpstr = "Target residue for paths calculation (see option -p)"
+    parser.add_argument("-b", "--target",
+                        dest = "target",
+                        default = None,
+                        type = int,
+                        help = b_helpstr)
 
     options = parser.parse_args()
 
+    # Load file
     matrix = np.loadtxt(options.input_matrix)
     graph = nx.Graph(matrix)
-    all_paths = get_shortest_paths(graph, 0, 139, options.maxl, options.res_space)
-    print(np.array(sort_paths(graph, all_paths, options.sort_by)))
+    all_paths = get_shortest_paths(graph = graph, \
+                                   source = options.source, \
+                                   target = options.target, \
+                                   maxl = options.maxl, \
+                                   res_space = options.res_space)
+    path_table = sort_paths(graph, all_paths, options.sort_by)
+
+    # Write file
+    with open("out.txt", "w") as f:
+        for path, length, sum_weight, avg_weight in path_table:
+            f.write(f"{path}\t{length}\t{sum_weight}\t{avg_weight}\n")
+
+
 
     #test graph
     #G = nx.Graph()
@@ -106,12 +136,7 @@ def main():
     
     #print(graph.nodes)
 
-    # Write file
-    #for path in all_paths:
-    #    print(path)
-    #with open("paths.txt", "w") as f:
-    #    for path in all_paths:
-    #        f.write('\n'.join(path))
+
 
 
 if __name__ == "__main__":
