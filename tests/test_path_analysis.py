4cf7cf8d5cf671830d37b82f82991ecc5b5f6022
mahdi-robbani <mahdi.robbani@gmail.com>
2021-01-29 16:11:54 +0100

    Added sb shortest path test

---
+++ tests/test_path_analysis.py
@@ -0,0 +1,83 @@
+import os
+import os.path
+import numpy as np
+import pytest
+from numpy.testing import assert_almost_equal, assert_equal
+from pyinteraph import path_analysis as pa
+
+# Load files
+@pytest.fixture
+def ref_dir(request):
+    return os.path.join(request.fspath.dirname, '../examples')
+
+@pytest.fixture
+def data_files(ref_dir):
+    return { 
+             'pdb' : os.path.join(ref_dir, 'sim.prot.twochains.pdb'),
+             'sb' : os.path.join(ref_dir, 'sb-graph_twochains_all.dat'),
+             'hc' : os.path.join(ref_dir, 'hc-graph.dat'),
+             'hb' : os.path.join(ref_dir, 'hb-graph.dat'),
+           }
+
+@pytest.fixture
+def sb_graph(data_files):
+    return pa.build_graph(data_files['sb'], data_files['pdb'])
+
+@pytest.fixture
+def sb_source(sb_graph):
+    return pa.convert_input_to_list(user_input = "A4:A6,A57,A58,A10",
+                                    identifiers = sb_graph[0])
+
+@pytest.fixture
+def sb_target(sb_graph):
+    return pa.convert_input_to_list(user_input = "A5,B1042",
+                                    identifiers = sb_graph[0])
+
+@pytest.fixture
+def sb_shortest_path(sb_graph, sb_source, sb_target):
+    return pa.get_shortest_paths(graph = sb_graph[1],
+                                 source = sb_source,
+                                 target = sb_target)
+
+# @pytest.fixture
+# def sb_shortest_path_graph()
+
+@pytest.fixture
+def sb_shortest_table(sb_graph, sb_shortest_path):
+    return pa.sort_paths(graph = sb_graph[1],
+                         paths = sb_shortest_path,
+                         sort_by = "length")
+
+@pytest.fixture
+def sb_ref_name(ref_dir):
+    return {
+             'shortest_csv' : os.path.join(ref_dir, 'shortest_paths.txt'),
+             'shortest_dat' : os.path.join(ref_dir, 'shortest_paths.dat')
+           }
+
+# @pytest.fixture
+# def sb_ref(sb_ref_name):
+#     return {
+#              'shortest_csv' : np.genfromtxt(sb_ref_name['shortest_csv'], delimiter=','),
+#              'shortest_dat' : np.loadtxt(sb_ref_name['shortest_dat'])
+#            }
+
+# Test shortest paths
+def test_shortest_path(sb_shortest_table, sb_ref_name):
+    ref_csv = []
+    with open(sb_ref_name['shortest_csv'], "r") as f:
+        for line in f:
+            # remove white space and split line
+            li, s, t, l, w1, w2 = line.rstrip().split('\t')
+            # change to correct format
+            line = (eval(li), s, t, int(l), float(w1), float(w2))
+            ref_csv.append(line)
+    assert sb_shortest_table == ref_csv
+
+
+
+
+# Test simple paths
+
+# Test metapath
+
