bbb376c5feabb1f81d07a9167a9cdbed650b563c
mahdi-robbani <mahdi.robbani@gmail.com>
2021-02-04 15:09:20 +0100

    Changed sb to scpsn

--- tests/test_path_analysis.py
+++ tests/test_path_analysis.py
@@ -15,89 +15,88 @@ def ref_dir(request):
 def data_files(ref_dir):
     return { 
              'pdb' : os.path.join(ref_dir, 'sim.prot.twochains.pdb'),
-             'sb' : os.path.join(ref_dir, 'sb-graph_twochains_all.dat')
-            #  'hc' : os.path.join(ref_dir, 'hc-graph.dat'),
-            #  'hb' : os.path.join(ref_dir, 'hb-graph.dat'),
+             'psn' : os.path.join(ref_dir, 'sc-graph_twochains_all.dat')
            }
 
 @pytest.fixture
-def sb_graph(data_files):
-    return pa.build_graph(data_files['sb'], data_files['pdb'])
+def data(data_files):
+    return pa.build_graph(data_files['psn'], data_files['pdb'])
 
 @pytest.fixture
-def sb_source(sb_graph):
-    return pa.convert_input_to_list(user_input = "A4:A6,A57,A58,A10",
-                                    identifiers = sb_graph[0])
+def source(data):
+    return pa.convert_input_to_list(user_input = "A2,A57",
+                                    identifiers = data[0])
 
 @pytest.fixture
-def sb_target(sb_graph):
-    return pa.convert_input_to_list(user_input = "A5,A11,B1042",
-                                    identifiers = sb_graph[0])
+def target(data):
+    return pa.convert_input_to_list(user_input = "B1042",
+                                    identifiers = data[0])
+
 
 @pytest.fixture
-def sb_shortest_path(sb_graph, sb_source, sb_target):
-    return pa.get_shortest_paths(graph = sb_graph[1],
-                                 source = sb_source,
-                                 target = sb_target)
+def shortest_path(data, source, target):
+    return pa.get_shortest_paths(graph = data[1],
+                                 source = source,
+                                 target = target)
 
 @pytest.fixture
-def sb_all_path(sb_graph, sb_source, sb_target):
-    return pa.get_all_simple_paths(graph = sb_graph[1],
-                                 source = sb_source,
-                                 target = sb_target,
+def all_path(data, source, target):
+    return pa.get_all_simple_paths(graph = data[1],
+                                 source = source,
+                                 target = target,
                                  maxl = 3)
 
 @pytest.fixture
-def sb_shortest_path_graph(sb_graph, sb_shortest_path):
-    return pa.get_persistence_graph(graph = sb_graph[1], 
-                                    paths = sb_shortest_path, 
-                                    identifiers = sb_graph[0])
+def shortest_path_graph(data, shortest_path):
+    return pa.get_persistence_graph(graph = data[1], 
+                                    paths = shortest_path, 
+                                    identifiers = data[0])
 
 @pytest.fixture
-def sb_all_path_graph(sb_graph, sb_all_path):
-    return pa.get_persistence_graph(graph = sb_graph[1], 
-                                    paths = sb_all_path, 
-                                    identifiers = sb_graph[0])
+def all_path_graph(data, all_path):
+    return pa.get_persistence_graph(graph = data[1], 
+                                    paths = all_path, 
+                                    identifiers = data[0])
 
 @pytest.fixture
-def sb_shortest_table(sb_graph, sb_shortest_path):
-    return pa.sort_paths(graph = sb_graph[1],
-                         paths = sb_shortest_path,
+def shortest_table(data, shortest_path):
+    return pa.sort_paths(graph = data[1],
+                         paths = shortest_path,
                          sort_by = "average_weight")
 
 @pytest.fixture
-def sb_all_table(sb_graph, sb_all_path):
-    return pa.sort_paths(graph = sb_graph[1],
-                         paths = sb_all_path,
+def all_table(data, all_path):
+    return pa.sort_paths(graph = data[1],
+                         paths = all_path,
                          sort_by = "average_weight")
                         
 @pytest.fixture
-def sb_metapath(sb_graph):
-    metapath = pa.get_metapath(graph = sb_graph[1],
-                               res_id = sb_graph[0],
+def metapath(data):
+    metapath = pa.get_metapath(graph = data[1],
+                               res_id = data[0],
                                res_space = 3,
                                node_threshold = 0.1,
                                edge_threshold = 0.1,
                                normalize = False)
-    metapath = pa.reorder_graph(metapath, sb_graph[0])
+    metapath = pa.reorder_graph(metapath, data[0])
     metapath = nx.to_numpy_matrix(metapath)
     return metapath
 
 @pytest.fixture
-def sb_metapath_norm(sb_graph):
-    metapath = pa.get_metapath(graph = sb_graph[1],
-                               res_id = sb_graph[0],
+def metapath_norm(data):
+    metapath = pa.get_metapath(graph = data[1],
+                               res_id = data[0],
                                res_space = 3,
                                node_threshold = 0.1,
                                edge_threshold = 0.1,
                                normalize = True)
-    metapath = pa.reorder_graph(metapath, sb_graph[0])
+    metapath = pa.reorder_graph(metapath, data[0])
     metapath = nx.to_numpy_matrix(metapath)
     return metapath
 
 
 @pytest.fixture
-def sb_ref_name(ref_dir):
+def ref_name(ref_dir):
     return {
              'shortest_csv' : os.path.join(ref_dir, 'shortest_paths.txt'),
              'shortest_dat' : os.path.join(ref_dir, 'shortest_paths.dat'),
@@ -109,45 +108,46 @@ def sb_ref_name(ref_dir):
 
 
 # Test shortest paths
-def test_shortest_path(sb_shortest_table, sb_ref_name):
+def test_shortest_path(shortest_table, ref_name):
     ref_csv = []
-    with open(sb_ref_name['shortest_csv'], "r") as f:
+    with open(ref_name['shortest_csv'], "r") as f:
         for line in f:
             # remove white space and split line
             li, s, t, l, w1, w2 = line.rstrip().split('\t')
             # change to correct format
             line = (eval(li), s, t, int(l), float(w1), float(w2))
             ref_csv.append(line)
-    assert sb_shortest_table == ref_csv
+    assert shortest_table == ref_csv
 
-def test_shortest_path_graph(sb_shortest_path_graph, sb_ref_name):
-    ref_graph = np.loadtxt(sb_ref_name['shortest_dat'])
-    graph = nx.to_numpy_matrix(sb_shortest_path_graph)
+def test_shortest_path_graph(shortest_path_graph, ref_name):
+    ref_graph = np.loadtxt(ref_name['shortest_dat'])
+    graph = nx.to_numpy_matrix(shortest_path_graph)
     assert_equal(graph, ref_graph)
 
 # Test simple paths
-def test_all_path(sb_all_table, sb_ref_name):
+def test_all_path(all_table, ref_name):
     ref_csv = []
-    with open(sb_ref_name['all_csv'], "r") as f:
+    with open(ref_name['all_csv'], "r") as f:
         for line in f:
             # remove white space and split line
             li, s, t, l, w1, w2 = line.rstrip().split('\t')
             # change to correct format
             line = (eval(li), s, t, int(l), float(w1), float(w2))
             ref_csv.append(line)
-    assert sb_all_table == ref_csv
+    assert all_table == ref_csv
 
-def test_all_path_graph(sb_all_path_graph, sb_ref_name):
-    ref_graph = np.loadtxt(sb_ref_name['all_dat'])
-    graph = nx.to_numpy_matrix(sb_all_path_graph)
+def test_all_path_graph(all_path_graph, ref_name):
+    ref_graph = np.loadtxt(ref_name['all_dat'])
+    graph = nx.to_numpy_matrix(all_path_graph)
     assert_equal(graph, ref_graph)
 
 # Test metapath
-def test_metapath(sb_metapath, sb_ref_name):
-    ref_metapath = np.loadtxt(sb_ref_name['metapath'])
-    assert_equal(sb_metapath, ref_metapath)
+def test_metapath(metapath, ref_name):
+    ref_metapath = np.loadtxt(ref_name['metapath'])
+    assert_equal(metapath, ref_metapath)
 
-def test_metapath_norm(sb_metapath_norm, sb_ref_name):
-    ref_metapath = np.loadtxt(sb_ref_name['metapath_norm'])
-    assert_almost_equal(sb_metapath_norm, ref_metapath)
+def test_metapath_norm(metapath_norm, ref_name):
+    ref_metapath = np.loadtxt(ref_name['metapath_norm'])
+    print((ref_metapath == metapath_norm).sum())
+    assert_almost_equal(metapath_norm, ref_metapath)
 
