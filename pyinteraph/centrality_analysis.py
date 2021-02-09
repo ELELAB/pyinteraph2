60ae2e4de82092a671f097d705e86c8fb0cdfd6c
mahdi-robbani <mahdi.robbani@gmail.com>
2021-02-09 11:28:52 +0100

    Added centrality dict

--- pyinteraph/centrality_analysis.py
+++ pyinteraph/centrality_analysis.py
@@ -40,9 +40,32 @@ def build_graph(fname, pdb = None):
     # return the idenfiers and the graph
     return identifiers, G
 
-def get_degree_cent(G, indentifiers):
-    pass
-    #x = nxc.degree_centrality(G)
+def get_degree_cent(G):
+    degree_dict = nxc.degree_centrality(G)
+    return degree_dict
+
+def get_betweeness_cent(G):
+    betweeness_dict = nxc.betweenness_centrality(G)
+    return betweeness_dict
+
+def get_centrality_dict(cent_name, function_map, graph):
+    """
+    Returns a dictionary where the key is the name of a centrality 
+    measure and the value is a dictionary of centrality values for each
+    node. e.g. {degree: {A: 0.1, B:0.7, ...}, betweenness: {...}}
+    """
+
+    centrality_dict = {}
+    if cent_name == "all":
+        # Add all centrality values to the dictionary
+        for key, func in function_map.items():
+            cent_dict = func(graph)
+            centrality_dict[key] = cent_dict
+    else:
+        # Only add specified values
+        cent_dict = function_map[cent_name](graph)
+        centrality_dict[key] = cent_dict
+    return centrality_dict
 
 
 def main():
@@ -65,7 +88,15 @@ def main():
                         default = None,
                         type = str)
 
-
+    c_choices = ["all", "degree", "betweenness"]
+    c_default = "all"
+    c_helpstr = "Select which centrality measure to calculate: " \
+                f"{c_choices} (default: {c_default}"
+    parser.add_argument("-c", "--centrality",
+                        dest = "cent",
+                        choices = c_choices,
+                        default = c_default,
+                        help =  c_helpstr)
 
     args = parser.parse_args()
 
@@ -90,7 +121,23 @@ def main():
 
     ############################ CENTRALITY ############################
 
-    get_degree_cent(graph, identifiers)
+    function_map = {'degree': get_degree_cent, 
+                    'betweenness': get_betweeness_cent}
+    
+    centrality_dict = get_centrality_dict(args.cent, function_map, graph)
+    for key, value in centrality_dict.items():
+        print(value)
+
+    # x = cent_func_map['degree'](graph)
+    # print(x)
+    # if args.cent == 'all':
+    #     for key, func in function_map.items():
+    #         cent_dict = func(graph)
+    #         cent_values.append(x)
+    # print(cent_values)
+    # degree_dict = get_degree_cent(graph)
+    # betweeness_dict = get_betweeness_cent(graph)
+    # print(betweeness_dict)
 
 
 if __name__ == "__main__":
