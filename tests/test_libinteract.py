1cd97906f9274d6c5778c7defbe80b6affa5d4d2
mahdi-robbani <mahdi.robbani@gmail.com>
2021-03-22 15:49:04 +0100

    Changed location of ref dir

--- tests/test_libinteract.py
+++ tests/test_libinteract.py
@@ -29,7 +29,7 @@ def hb_file():
 
 @pytest.fixture
 def ref_dir(request):
-    return os.path.join(request.fspath.dirname, '../examples')
+    return os.path.join(request.fspath.dirname, 'data/single_chain')
 
 @pytest.fixture
 def ref_dir_twochains(request):
