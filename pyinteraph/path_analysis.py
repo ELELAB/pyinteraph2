d455ff90e093142ae26fb500d0e714ac1c03b3fc
Matteo Tiberti <matteo.tiberti@gmail.com>
2023-01-06 11:18:31 +0100

    changed new function so that it returns an output

--- pyinteraph/path_analysis.py
+++ pyinteraph/path_analysis.py
@@ -484,37 +484,37 @@ def reorder_graph(graph, identifiers):
     ordered_graph.add_weighted_edges_from(weighted_edges)
     return(ordered_graph)
 
-def write_metapath_table(graph, identifiers, residue_names, fname, normalize):
-        '''The function writes a file csv containing the information
-        about the edges of the metapath graph and their weight.'''
+def generate_metapath_table(graph, identifiers, residue_names, normalize):
+    '''The function returns a string formatted as a csv file, containing
+    the information about the edges of the metapath graph and their weight.'''
 
-        # Create dictionary of {ids : resnames}
-        nodes_dict = dict(zip(identifiers, residue_names))
-        
-        with open(fname, 'w') as out:
-            
-            # Write header depending on whether the recurrency
-            # is normalized or not
-            if normalize == True:
-                w_label = 'relative_recurrency'
-            else:
-                w_label = 'recurrency'
-            out.write(f'chain1,resid1,restype1,group1,' \
-                      f'chain2,resid2,restype2,group2,{w_label}\n')
+    # Create dictionary of {ids : resnames}
+    nodes_dict = dict(zip(identifiers, residue_names))
 
-            # Define chain, resid, and resname for each node 
-            # of each edge, and the edge weight
-            for node1, node2, w in graph.edges(data=True):
-                chain1, chain2 = node1[0], node2[0]
-                resid1, resid2 = int(''.join([x for x in list(node1) if x.isdigit()])), \
-                                 int(''.join([x for x in list(node2) if x.isdigit()]))
-                restype1, restype2 = nodes_dict[node1], nodes_dict[node2]
-                e_weight = w['weight']
+    # Write header depending on whether the recurrency
+    # is normalized or not
+    if normalize == True:
+        w_label = 'relative_recurrency'
+    else:
+        w_label = 'recurrency'
 
-                # Write output
-                out.write(f'{chain1},{resid1},{restype1},sidechain,' \
-                          f'{chain2},{resid2},{restype2},sidechain,{e_weight}\n')
+    out = f'chain1,resid1,restype1,group1,' \
+            f'chain2,resid2,restype2,group2,{w_label}\n'
 
+    # Define chain, resid, and resname for each node
+    # of each edge, and the edge weight
+    for node1, node2, w in graph.edges(data=True):
+        chain1, chain2 = node1[0], node2[0]
+        resid1, resid2 = int(''.join([x for x in list(node1) if x.isdigit()])), \
+                            int(''.join([x for x in list(node2) if x.isdigit()]))
+        restype1, restype2 = nodes_dict[node1], nodes_dict[node2]
+        e_weight = w['weight']
+
+        # Write output
+        out += f'{chain1},{resid1},{restype1},sidechain,' \
+                f'{chain2},{resid2},{restype2},sidechain,{e_weight}\n'
+
+    return out
 
 def main():
 
@@ -776,7 +776,7 @@ def main():
             args.m_out = os.path.splitext(args.m_out)[0]
             # Plot graph
             plot_graph(fname = f"{args.m_out}", 
-                       graph = metapath_graph, 
+                       graph = metapath_graph,
                        hub_num = args.hub,
                        col_map_e = "rocket_r",
                        col_map_n = "gray_r",
@@ -793,12 +793,13 @@ def main():
             np.savetxt(f"{args.m_out}.dat", metapath_matrix)
 
             # Save metapath csv
-            write_metapath_table(graph = metapath_graph, 
-                                 identifiers = identifiers,
-                                 residue_names = residue_names,
-                                 normalize = args.do_normalize,
-                                 fname = f"{args.m_out}.csv")
+            table = generate_metapath_table(graph = metapath_graph, 
+                                           identifiers = identifiers,
+                                           residue_names = residue_names,
+                                           normalize = args.do_normalize)
 
+            with open(f"{args.m_out}.csv", 'w') as fh:
+                fh.write(table)
 
 if __name__ == "__main__":
     main()
