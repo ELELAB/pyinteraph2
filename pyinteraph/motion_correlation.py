cc72d10113cc6356cccba3110d16592f98323510
vinitlol <vasavinit@gmail.com>
2026-05-08 13:16:14 +0200

    Changed according to comments and added a csv output

--- pyinteraph/motion_correlation.py
+++ pyinteraph/motion_correlation.py
@@ -30,9 +30,9 @@ def main():
     parser = argparse.ArgumentParser(description="Compute correlation metrics from MD trajectory")
 
     parser.add_argument(
-        "-r", "--ref",
+        "-s", "--top",
         required = True,
-        help = "Reference PDB file"
+        help = "Topology file"
     )
 
     parser.add_argument(
@@ -55,40 +55,67 @@ def main():
     )
 
     parser.add_argument(
-        "-s", "--select",
+        "-sel", "--select",
         default = "name CA",
         help = "Atom selection for DCCM/LMI calculation (default: name CA)"
+    )
+
+    parser.add_argument(
+        "-o", "--output",
+        default = "dccm",
+        help = "Output file name (default: dccm.dat)"
     )
 
     args = parser.parse_args()
 
     # Loading the structure and trajectory
     try:
-        u = mda.Universe(args.ref, args.trj)
+        u = mda.Universe(args.top, args.trj, in_memory=True)
     except FileNotFoundError:
         print("Error: One or more files not found")
         sys.exit(1)
     except ValueError:
         print("Error: Invalid file format for pdb or trajectory")
         sys.exit(1)
+    except OSError:
+        print("Error: One or more files not found")
+        sys.exit(1)
 
     # Aligning the frames w.r.t user input (Default: name CA)
-    ref = mda.Universe(args.ref)    # Aligning to the reference structure
+    ref = mda.Universe(args.top)    # Aligning to the reference structure
     align.AlignTraj(u, ref, select=args.align, in_memory=True).run()
 
     atoms = u.select_atoms(args.select)
-    coords = []
-    for ts in u.trajectory:
-        coords.append(atoms.positions.copy())
-    coords = np.array(coords)
-
+    coords = u.trajectory.timeseries(atoms, order="fac")    # so that the shape is (frames, atoms, coords)
     fluct = fluct_calc(coords)
 
     if args.method == "dccm":         # lmi to be added
         result = dccm_calc(fluct)
-        output_file = "dccm.dat"
+        output_file = args.output + ".dat"
 
     np.savetxt(output_file, result, delimiter=" ")  # writing to a .dat file
+
+    # Writing a CSV file with residue pairs
+    csv_output = output_file.replace(".dat", ".csv")
+
+    with open(csv_output, "w") as outfile:
+        outfile.write("chain1,residue_number1,residue_name1,atom1,"
+                       "chain2,residue_number2,residue_name2,atom2,correlation\n"
+        )
+        for i in range(len(atoms)):
+            for j in range(i+1, len(atoms)):
+
+                outfile.write(
+                    f"{atoms[i].chainID},"
+                    f"{atoms[i].resid},"
+                    f"{atoms[i].resname},"
+                    f"{atoms[i].name},"
+                    f"{atoms[j].chainID},"
+                    f"{atoms[j].resid},"
+                    f"{atoms[j].resname},"
+                    f"{atoms[j].name},"
+                    f"{result[i,j]:.6f}\n"
+                )
 
 if __name__ == "__main__":
     main()
